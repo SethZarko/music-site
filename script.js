8014e61104a2e96eaf0b305f154c43e51a251e42
SethZarko <sethzarkovich@gmail.com>
2022-01-27 14:50:23 -0500

    Styling and Making Nav Interactive and Dynamic

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// Dynamic Navigation Underline on Sroll //
+
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav ul li');
+
+window.addEventListener('scroll', ()=> {
+    let current = '';
+    sections.forEach( section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(pageYOffset >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute('id');
+        }
+    })
+
+    navLi.forEach( li => {
+       li.classList.remove('active'); 
+       if(li.classList.contains(current)) {
+        li.classList.add('active')
+       }
+    })
+});
